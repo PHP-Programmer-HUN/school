be048901df58ff6ff6f508cb0361fe7dc5419a2c
Varga Levente István <47919682+Varga-Levente@users.noreply.github.com>
2021-02-21 12:55:04 +0100

    FINAL

--- matyi.java
+++ matyi.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
@@ -15,8 +17,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.text.NumberFormatter;
+import java.util.IntSummaryStatistics;
 
-public class Main implements ActionListener{
+public class Main{
 	
 	public Main(){
 		//Keret felépítése
@@ -49,9 +52,6 @@ public class Main implements ActionListener{
         
         Iterator<Sor> itr=al.iterator();
         
-        //Prices
-        int[] prices = new int[]{250, 160, 220, 250, 120, 180, 90}; 
-        
         //JFormattedTextField (Number formatted text fields)
 		NumberFormat longFormat = NumberFormat.getIntegerInstance();
 		NumberFormatter numberFormatter = new NumberFormatter(longFormat);
@@ -66,7 +66,6 @@ public class Main implements ActionListener{
 		lt_t1.setText(st.ar);
 		JLabel jl_c1 = new JLabel(currency);
 		JButton jb_b1 = new JButton(st.hetnap);
-		jb_b1.addActionListener(this);
 		
 		st=(Sor)itr.next();
 		JLabel jl_l2 = new JLabel(st.virag);
@@ -74,7 +73,6 @@ public class Main implements ActionListener{
 		lt_t2.setText(st.ar);
 		JLabel jl_c2 = new JLabel(currency);
 		JButton jb_b2 = new JButton(st.hetnap);
-		jb_b2.addActionListener(this);
 		
 		st=(Sor)itr.next();
 		JLabel jl_l3 = new JLabel(st.virag);
@@ -82,7 +80,6 @@ public class Main implements ActionListener{
 		lt_t3.setText(st.ar);
 		JLabel jl_c3 = new JLabel(currency);
 		JButton jb_b3 = new JButton(st.hetnap);
-		jb_b3.addActionListener(this);
 		
 		st=(Sor)itr.next();
 		JLabel jl_l4 = new JLabel(st.virag);
@@ -90,7 +87,6 @@ public class Main implements ActionListener{
 		lt_t4.setText(st.ar);
 		JLabel jl_c4 = new JLabel(currency);
 		JButton jb_b4 = new JButton(st.hetnap);
-		jb_b4.addActionListener(this);
 		
 		st=(Sor)itr.next();
 		JLabel jl_l5 = new JLabel(st.virag);
@@ -98,7 +94,6 @@ public class Main implements ActionListener{
 		lt_t5.setText(st.ar);
 		JLabel jl_c5 = new JLabel(currency);
 		JButton jb_b5 = new JButton(st.hetnap);
-		jb_b5.addActionListener(this);
 		
 		st=(Sor)itr.next();
 		JLabel jl_l6 = new JLabel(st.virag);
@@ -106,7 +101,6 @@ public class Main implements ActionListener{
 		lt_t6.setText(st.ar);
 		JLabel jl_c6 = new JLabel(currency);
 		JButton jb_b6 = new JButton(st.hetnap);
-		jb_b6.addActionListener(this);
 		
 		st=(Sor)itr.next();
 		JLabel jl_l7 = new JLabel(st.virag);
@@ -114,7 +108,6 @@ public class Main implements ActionListener{
 		lt_t7.setText(st.ar);
 		JLabel jl_c7 = new JLabel(currency);
 		JButton jb_b7 = new JButton(st.hetnap);
-		jb_b7.addActionListener(this);
 		
 		JButton jb_s1 = new JButton("Beállítás");
 		JButton jb_s2 = new JButton("Melyik a legolcsóbb?");
@@ -189,6 +182,8 @@ public class Main implements ActionListener{
 		
 		jt_sum.setEditable(false);
 		
+		jb_s3.setEnabled(false);
+		
 		//Elemek formázása
 		lt_t1.setHorizontalAlignment(JTextField.RIGHT);
 		lt_t2.setHorizontalAlignment(JTextField.RIGHT);
@@ -199,6 +194,8 @@ public class Main implements ActionListener{
 		lt_t7.setHorizontalAlignment(JTextField.RIGHT);
 		
 		jl_sum.setFont(new Font("Ubuntu", Font.BOLD, 15));
+		jt_sum.setFont(new Font("Ubuntu", Font.BOLD, 15));
+		jt_sum.setHorizontalAlignment(JTextField.RIGHT);
 		jl_sum_currency.setFont(new Font("Ubuntu", Font.BOLD, 15));
 		
 		//Ablakhoz adás
@@ -271,6 +268,7 @@ public class Main implements ActionListener{
 					jb_b7.setEnabled(false);
 					jb_s1.setEnabled(false);
 					jb_s2.setEnabled(false);
+					jb_s3.setEnabled(true);
 					JLabel msgLabel = new JLabel("Mostantól változtathatóak az árak. \n Az új árak alkalmazásához kattintson az alkalmaz gombra!", JLabel.CENTER);
 					JOptionPane.showMessageDialog(null, msgLabel, "Információ", JOptionPane.INFORMATION_MESSAGE);
 				}
@@ -297,46 +295,219 @@ public class Main implements ActionListener{
 				jb_b7.setEnabled(true);
 				jb_s1.setEnabled(true);
 				jb_s2.setEnabled(true);
-				prices[0] = Integer.parseInt(lt_t1.getText());
-				prices[1] = Integer.parseInt(lt_t2.getText());
-				prices[2] = Integer.parseInt(lt_t3.getText());
-				prices[3] = Integer.parseInt(lt_t4.getText());
-				prices[4] = Integer.parseInt(lt_t5.getText());
-				prices[5] = Integer.parseInt(lt_t6.getText());
-				prices[6] = Integer.parseInt(lt_t7.getText());
+				jb_s3.setEnabled(false);
 			}
 		});
-		
+		jb_b1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				jt_sum.setText(Integer.toString((h1*v1)+(h2*v2)+(h3*v3)+(h4*v4)+(h5*v5)+(h6*v6)+(h7*v7)));
+			}
+		});
+		jb_b2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				jt_sum.setText(Integer.toString((h2*v1)+(h3*v2)+(h4*v3)+(h5*v4)+(h6*v5)+(h7*v6)+(h1*v7)));
+			}
+		});
+		jb_b3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				jt_sum.setText(Integer.toString((h3*v1)+(h4*v2)+(h5*v3)+(h6*v4)+(h7*v5)+(h1*v6)+(h2*v7)));
+			}
+		});
+		jb_b4.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				jt_sum.setText(Integer.toString((h4*v1)+(h5*v2)+(h6*v3)+(h7*v4)+(h1*v5)+(h2*v6)+(h3*v7)));
+			}
+		});
+		jb_b5.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				jt_sum.setText(Integer.toString((h5*v1)+(h6*v2)+(h7*v3)+(h1*v4)+(h2*v5)+(h3*v6)+(h4*v7)));
+			}
+		});
+		jb_b6.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				jt_sum.setText(Integer.toString((h6*v1)+(h7*v2)+(h1*v3)+(h2*v4)+(h3*v5)+(h4*v6)+(h5*v7)));
+			}
+		});
+		jb_b7.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				jt_sum.setText(Integer.toString((h7*v1)+(h1*v2)+(h2*v3)+(h3*v4)+(h4*v5)+(h5*v6)+(h6*v7)));
+			}
+		});
+		jb_s2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int h1 = jb_b1.getText().length();
+				int v1 = Integer.parseInt(lt_t1.getText());
+				int h2 = jb_b2.getText().length();
+				int v2 = Integer.parseInt(lt_t2.getText());
+				int h3 = jb_b3.getText().length();
+				int v3 = Integer.parseInt(lt_t3.getText());
+				int h4 = jb_b4.getText().length();
+				int v4 = Integer.parseInt(lt_t4.getText());
+				int h5 = jb_b5.getText().length();
+				int v5 = Integer.parseInt(lt_t5.getText());
+				int h6 = jb_b6.getText().length();
+				int v6 = Integer.parseInt(lt_t6.getText());
+				int h7 = jb_b7.getText().length();
+				int v7 = Integer.parseInt(lt_t7.getText());
+				Summary sum1=new Summary("Hétfő", (h1*v1)+(h2*v2)+(h3*v3)+(h4*v4)+(h5*v5)+(h6*v6)+(h7*v7));
+				Summary sum2=new Summary("Kedd", (h2*v1)+(h3*v2)+(h4*v3)+(h5*v4)+(h6*v5)+(h7*v6)+(h1*v7));
+				Summary sum3=new Summary("Szerda", (h3*v1)+(h4*v2)+(h5*v3)+(h6*v4)+(h7*v5)+(h1*v6)+(h2*v7));
+				Summary sum4=new Summary("Csütörtök", (h4*v1)+(h5*v2)+(h6*v3)+(h7*v4)+(h1*v5)+(h2*v6)+(h3*v7));
+				Summary sum5=new Summary("Péntek", (h5*v1)+(h6*v2)+(h7*v3)+(h1*v4)+(h2*v5)+(h3*v6)+(h4*v7));
+				Summary sum6=new Summary("Szombat", (h6*v1)+(h7*v2)+(h1*v3)+(h2*v4)+(h3*v5)+(h4*v6)+(h5*v7));
+				Summary sum7=new Summary("Vasárnap", (h7*v1)+(h1*v2)+(h2*v3)+(h3*v4)+(h4*v5)+(h5*v6)+(h6*v7));
+				ArrayList<Summary> sumdata=new ArrayList<Summary>();
+				
+				sumdata.add(sum1);
+				//System.out.println(sum1.sum);
+				sumdata.add(sum2);
+				sumdata.add(sum3);
+				sumdata.add(sum4);
+				sumdata.add(sum5);
+				sumdata.add(sum6);
+				sumdata.add(sum7);
+		        
+		        Iterator<Summary> itr=sumdata.iterator();
+		        String minhet = "";
+		        int min = (h1*v1)+(h2*v2)+(h3*v3)+(h4*v4)+(h5*v5)+(h6*v6)+(h7*v7);
+		        while (itr.hasNext()) {
+					Summary summary = (Summary) itr.next();
+					if (summary.sum<min) {
+						min = summary.sum;
+						minhet = summary.hetnapja;
+					}
+				}
+
+				JLabel msgLabel = new JLabel("Legolcsóbb ha "+minhet+" napon kéri meg Pöttöm Panna kezét, "+min+" "+currency+"-ba kerül.", JLabel.CENTER);
+				JOptionPane.showMessageDialog(null, msgLabel, "Információ", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		jb_s4.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+				
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
 		Main main = new Main();
-	}
-	
-	public void actionPerformed(ActionEvent e){
-		switch(e.getActionCommand()){
-			case "Hétfő":
-				System.out.println("Hétfő");
-				break;
-			case "Kedd":
-				System.out.println("Kedd");
-				break;
-			case "Szerda":
-				System.out.println("Szerda");
-				break;
-			case "Csütörtök":
-				System.out.println("Csütörtök");
-				break;
-			case "Péntek":
-				System.out.println("Péntek");
-				break;
-			case "Szombat":
-				System.out.println("Szombat");
-				break;
-			case "Vasárnap":
-				System.out.println("Vasárnap");
-				break;
-		}
 	}
 }
 
@@ -349,4 +520,18 @@ class Sor{
         this.ar=ar;  
         this.hetnap=hetnap;  
     }  
-} 
+}
+class Fprice{  
+	int price;
+    Fprice(int price){  
+        this.price=price;
+    }  
+}
+class Summary{
+	String hetnapja;
+	int sum;
+	Summary(String hetnapja, int sum){  
+        this.sum=sum;
+        this.hetnapja=hetnapja;
+    }  
+}
